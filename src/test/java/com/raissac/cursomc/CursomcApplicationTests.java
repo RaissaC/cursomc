9f8a9cecc6d4f065893c9dd24558aae1bff93531
Raissa Cesar <raissa.soares.cesar@gmail.com>
2022-03-10 12:14:52 -0300

    projeto criado

---
+++ src/test/java/com/raissac/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raissac.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
